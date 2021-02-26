30eb1f604b22bb7e0ad38ca0e4f0137b3b1ed7f8
nchernyshov <xpom55@mail.ru>
2021-02-26 16:41:31 +0300

    feat: simple NN with my Weights and my input data

--- ai.py
+++ ai.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 
 class Neural_Network(nn.Module):
 
-  def __init__(self, weights=None):
+  def __init__(self, weights=None, layers=[2,3,1]):
     super(Neural_Network, self).__init__()
     # parameters
     # TODO: parameters can be parameterized instead of declaring them here
-    self.inputSize = 2
-    self.hiddenSize = 3
-    self.outputSize = 1
+    self.inputSize = layers[0]
+    self.hiddenSize = layers[1]
+    self.outputSize = layers[2]
 
     # weights
     if weights is not None:
@@ -55,7 +55,7 @@ class Neural_Network(nn.Module):
     # forward + backward pass for training
     o = self.forward(X)
     self.backward(X, y, o)
-    self.xPredicted = o
+    # self.xPredicted = o
 
   def saveWeights(self, model):
     # we will use the PyTorch internal storage functions
@@ -71,38 +71,38 @@ class Neural_Network(nn.Module):
 class AI:
 
   def __init__(self):
-    self.W1 = torch.tensor(([[0, 1]]), dtype=torch.float)
-    self.W2 = torch.tensor(([[0], [1]]), dtype=torch.float)
+    # 2 x 3 (2 input to 3 hidden nodes)
+    self.W1 = torch.tensor(([[1, 1,0.4], [1,1,0.1]]), dtype=torch.float)
+    # 3 x 1 (3 hidden to 1 output)
+    self.W2 = torch.tensor(([[0.1], [1], [1]]), dtype=torch.float)
 
-    # NN = Neural_Network([self.W1, self.W2])
-    NN = Neural_Network()
+    NN = Neural_Network([self.W1, self.W2])
+    # NN = Neural_Network()
 
-    self.X = torch.tensor(([0]), dtype=torch.float)
+    self.X = torch.tensor(([[1,1]]), dtype=torch.float)
 
-    # print(NN.forward(self.X))
-
-    self.lr = 0.01
+    print(NN.forward(self.X))
 
     # 3 X 2 tensor
-    X = torch.tensor(([2, 9], [1, 5], [3, 6]), dtype=torch.float)
+    # X = torch.tensor(([2, 9], [1, 5], [3, 6]), dtype=torch.float)
     # 3 X 1 tensor
-    y = torch.tensor(([92], [100], [89]), dtype=torch.float)
+    # y = torch.tensor(([92], [100], [89]), dtype=torch.float)
     # 1 X 2 tensor
-    xPredicted = torch.tensor(([4, 8]), dtype=torch.float)
+    # xPredicted = torch.tensor(([4, 8]), dtype=torch.float)
 
 
     # scale units [0:1]
-    X_max, _ = torch.max(X, 0)
-    xPredicted_max, _ = torch.max(xPredicted, 0)
+    # X_max, _ = torch.max(X, 0)
+    # xPredicted_max, _ = torch.max(xPredicted, 0)
 
-    X = torch.div(X, X_max)
-    xPredicted = torch.div(xPredicted, xPredicted_max)
-    y = y / 100  # max test score is 100
+    # X = torch.div(X, X_max)
+    # xPredicted = torch.div(xPredicted, xPredicted_max)
+    # y = y / 100  # max test score is 100
 
-    for i in range(1000):  # trains the NN 1,000 times
-      print("#" + str(i) + " Loss: " +
-            str(torch.mean(
-                (y - NN(X))**2).detach().item()))  # mean sum squared loss
-      NN.train(X, y)
+    # for i in range(1000):  # trains the NN 1,000 times
+      # print("#" + str(i) + " Loss: " +
+            # str(torch.mean(
+                # (y - NN(X))**2).detach().item()))  # mean sum squared loss
+      # NN.train(X, y)
 
-    NN.predict()
+    # NN.predict()
